94c1c8228cec58bb3f261f421a33adf1822bb9f5
me0wmerz <rachel.hoelscher@gmail.com>
2021-11-23 10:16:55 -0800

    set up files

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+//API for recipes
+
+//API for calories
+  // const options = {
+  //   method: 'GET',
+  //   url: 'https://food-calorie-data-search.p.rapidapi.com/api/search',
+  //   params: { keyword: 'chicken' },
+  //   headers: {
+  //     'x-rapidapi-key': 'e867de6562mshdf4837fcdae9882p1cb737jsn366b080e13f2',
+  //     'x-rapidapi-host': 'food-calorie-data-search.p.rapidapi.com'
+  //   }
+  // };
+
+//API for beer/cocktail pairings 
+
+//API for related YouTube videos 
+
+//built-in timer? for user to start/stop time and have multiple timers based on the time it takes?
