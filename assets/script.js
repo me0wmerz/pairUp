9aba0a39488c715905334fc098dafa68bc7d3565
Chanwoo Hwang <Chanwoo1017@hotmail.com>
2021-11-24 18:43:34 -0800

    change some js

--- assets/script.js
+++ assets/script.js
@@ -31,28 +31,43 @@ const apiKey ="721aa72fafd34a0788bd54bc3d53543c"
 document.getElementById("btn").addEventListener("click", event =>{
  event.preventDefault()
  const ingredientsEl = document.getElementById("ingredients").value
-  axios.get(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${ingredientsEl}&number=4`)
+  axios.get(` https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&instructionsRequired=true&addRecipeInformation=true&addRecipeNutrition=true&includeIngredients=${ingredients}&number=4`)
   .then(res => {  
-    // console.log(res)
-    for (let i = 0; i < 4; i++) {
-      let foodInfo = res.data[i]
-      // console.log(foodInfo.title)
-      // console.log(foodInfo.image)
-
-
-      document.getElementById('title-1').innerHTML = `
-        <h5> ${foodInfo.title} </h5>
-      
-      `  
-      document.getElementById('image-1').innerHTML=`
+    console.log(res)
+    for (let indexRecipe = 0; indexRecipe < 4; indexRecipe++) {
+      let foodInfo = res.data.results[indexRecipe]
+      console.log(foodInfo.title)
+      console.log(foodInfo.image)
+      const foodnameElem = document.createElement("div")
+      foodnameElem.className = "title-1"
+      if (foodInfo.title){
+        foodnameElem.innerHTML=`
+        <h3>${foodInfo.title}</h3>
         <img src="${foodInfo.image}" alt="${foodInfo.title}">
+        
+        `
+        document.getElementById("title-1").append(foodnameElem)
+        document.getElementById("image-1").append(foodnameElem)
+      }
 
-      `
-       
-    
-      
+      // console.log(foodInfo.summary)
+      // console.log(foodInfo.readyInMinutes)
+      // console.log(foodInfo.servings)
+      // //fix below - need to display all in array
+      // console.log(foodInfo.cuisines[0])
+      // //fix below - need to display all in array
+      // let instructions = console.log(foodInfo.analyzedInstructions[0])
+      // console.log(foodInfo.analyzedInstructions[0])
+      // for (let indexIngredient = 0; indexIngredient < 4; indexIngredient++) {
+      //   console.log(foodInfo.usedIngredients[indexIngredient].originalString)
+      //   console.log(foodInfo.usedIngredients[indexIngredient].image)
+      //   console.log(foodInfo.missedIngredients[indexIngredient].originalString)
+      //   console.log(foodInfo.missedIngredients[indexIngredient].image)
+      //   //missing instructions
+      // }
     }
-})
-
-  .catch(err => console.log(err))
+  })
+    .catch(err =>
+      console.log(err))
+   
 })
