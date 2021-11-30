1dfa43936e6831c7c65b3e3f2f038dfe61f212b5
me0wmerz <rachel.hoelscher@gmail.com>
2021-11-30 13:32:42 -0800

    final changes except styling for "recipie" page

--- assets/script.js
+++ assets/script.js
@@ -1,53 +1,53 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
- // Get all "navbar-burger" elements
- const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
 
- // Check if there are any navbar burgers
- if ($navbarBurgers.length > 0) {
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
 
-  // Add a click event on each of them
-  $navbarBurgers.forEach(el => {
-   el.addEventListener('click', () => {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
 
-    // Get the target from the "data-target" attribute
-    const target = el.dataset.target;
-    const $target = document.getElementById(target);
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
 
-    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    el.classList.toggle('is-active');
-    $target.classList.toggle('is-active');
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
 
-   });
-  });
- }
+      });
+    });
+  }
 });
 
 // make search button for searching food 
-const apiKey ="721aa72fafd34a0788bd54bc3d53543c"
-              
+const apiKey = "721aa72fafd34a0788bd54bc3d53543c"
+
 const inputs = document.querySelectorAll('input')
 
 
 
-document.getElementById("btn").addEventListener("click", event =>{
- event.preventDefault()
- const ingredientsEl = document.getElementById("ingredients").value
- 
-  axios.get(` https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&instructionsRequired=true&addRecipeInformation=true&addRecipeNutrition=true&includeIngredients=${ingredients}&number=6`)
-  .then(res => {  
-    console.log(res)
-    for (let indexRecipe = 0; indexRecipe < 6; indexRecipe++) {
-      let foodInfo = res.data.results[indexRecipe]
-      console.log(foodInfo.title)
-      console.log(foodInfo.image)
-      
-      const foodnameElem = document.createElement("div")
-      foodnameElem.className = "title-1"
-      if (foodInfo.title){
+document.getElementById("btn").addEventListener("click", event => {
+  event.preventDefault()
+  const ingredientsEl = document.getElementById("ingredients").value
 
-        foodnameElem.innerHTML=`
+  axios.get(` https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&instructionsRequired=true&addRecipeInformation=true&addRecipeNutrition=true&includeIngredients=${ingredients}&number=6`)
+    .then(res => {
+      console.log(res)
+      for (let indexRecipe = 0; indexRecipe < 6; indexRecipe++) {
+        let foodInfo = res.data.results[indexRecipe]
+        console.log(foodInfo.title)
+        console.log(foodInfo.image)
+
+        const foodnameElem = document.createElement("div")
+        foodnameElem.className = "title-1"
+        if (foodInfo.title) {
+
+          foodnameElem.innerHTML = `
           <div class="card" >
           <h3>${foodInfo.title}</h3>
           <img src="${foodInfo.image}" alt="${foodInfo.title}">
@@ -58,14 +58,14 @@ document.getElementById("btn").addEventListener("click", event =>{
 
           
         `
-        document.getElementById("food-info").append(foodnameElem)
-        // document.getElementById("image-1").append(foodnameElem)
-       
-      }
+          document.getElementById("food-info").append(foodnameElem)
+          // document.getElementById("image-1").append(foodnameElem)
 
-    }
-  })
+        }
+
+      }
+    })
     .catch(err =>
       console.log(err))
-   
+
 })
--- recipe.js
+++ recipe.js
@@ -1,89 +1,109 @@
-//  API for cocktails
-axios.get(`https://www.thecocktaildb.com/api/json/v1/1/random.php`)
-  .then(res => {
-    const drinkInfo = res.data.drinks[0]
-    document.getElementById('drinkName').innerHTML = `
-      <h2>${drinkInfo.strDrink}</h2>
-      <img src=${drinkInfo.strDrinkThumb} alt=${drinkInfo.strDrink} pic" width="250" >
-      `
+//BELOW IS FOR FOOD SECTION
 
+// make search button for searching food 
+const apiKey = "3f5180d6774b495eb794eb02a08d8db6"
 
-    //setting array of objects for key/value pair since API does not format it in such a way
-    let drinkList = [
-      {
-        measure: drinkInfo.strMeasure1,
-        ingredient: drinkInfo.strIngredient1
-      },
-      {
-        measure: drinkInfo.strMeasure2,
-        ingredient: drinkInfo.strIngredient2
-      },
-      {
-        measure: drinkInfo.strMeasure3,
-        ingredient: drinkInfo.strIngredient3
-      },
-      {
-        measure: drinkInfo.strMeasure3,
-        ingredient: drinkInfo.strIngredient3
-      },
-      {
-        measure: drinkInfo.strMeasure4,
-        ingredient: drinkInfo.strIngredient4
-      },
-      {
-        measure: drinkInfo.strMeasure5,
-        ingredient: drinkInfo.strIngredient5
-      },
-      {
-        measure: drinkInfo.strMeasure6,
-        ingredient: drinkInfo.strIngredient6
-      },
-      {
-        measure: drinkInfo.strMeasure7,
-        ingredient: drinkInfo.strIngredient7
-      },
-      {
-        measure: drinkInfo.strMeasure8,
-        ingredient: drinkInfo.strIngredient8
-      },
-      {
-        measure: drinkInfo.strMeasure9,
-        ingredient: drinkInfo.strIngredient9
-      },
-      {
-        measure: drinkInfo.strMeasure10,
-        ingredient: drinkInfo.strIngredient10
-      },
-      {
-        measure: drinkInfo.strMeasure11,
-        ingredient: drinkInfo.strIngredient11
-      },
-      {
-        measure: drinkInfo.strMeasure12,
-        ingredient: drinkInfo.strIngredient12
-      },
-      {
-        measure: drinkInfo.strMeasure13,
-        ingredient: drinkInfo.strIngredient13
-      },
-      {
-        measure: drinkInfo.strMeasure14,
-        ingredient: drinkInfo.strIngredient14
-      },
-      {
-        measure: drinkInfo.strMeasure15,
-        ingredient: drinkInfo.strIngredient15
-      }
-    ]
+const inputs = document.querySelectorAll('input')
 
-    drinkList.forEach((cocktailIngredient, i) => {
-      const drinkElem = document.createElement('p')
-      drinkElem.className = "cocktailIngredient"
+// let ingredients = "egg"
 
-      if (cocktailIngredient.measure) {
-        drinkElem.innerHTML = `
-          <li>${cocktailIngredient.measure} ${cocktailIngredient.ingredient}</li>
+document.getElementById("search").addEventListener("click", event => {
+  event.preventDefault()
+  let ingredientsEl = document.getElementById("userInput").value
+
+  axios.get(` https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&instructionsRequired=true&addRecipeInformation=true&addRecipeNutrition=true&includeIngredients=${ingredientsEl}&number=1`)
+    .then(res => {
+      console.log(res)
+      //for basic recipe info
+      for (let indexRecipe = 0; indexRecipe < 1; indexRecipe++) {
+        let foodInfo = res.data.results[indexRecipe]
+        // console.log(foodInfo.title)
+        // console.log(foodInfo.image)
+        // console.log(foodInfo.summary)
+        // console.log(foodInfo.readyInMinutes)
+        // console.log(foodInfo.servings)
+        //displays all cuisine types in array (below)
+        //let cuisineTypes = foodInfo.cuisines.join(", ")
+
+        document.getElementById('foodName').innerHTML = `
+              <h2>${foodInfo.title}</h2>
+              <br>
+              `
+        document.getElementById('foodPic').innerHTML = `
+              <img src=${foodInfo.image} alt=${foodInfo.title} picture" width="350">
+              <br>
+            `
+        document.getElementById('totalTime').innerHTML = `
+              Ready In: ${foodInfo.readyInMinutes} minutes
+              `
+        document.getElementById('servings').innerHTML = `
+              Serving Size: ${foodInfo.servings} 
+              `
+        ///ADD STYLING (WRAP AROUND PIC, OR SCROLL-DOWN ON BULMA)
+        document.getElementById('summary').innerHTML = `
+              ${foodInfo.summary} 
+              `
+
+        // for (let indexRecipe = 0; indexRecipe < 1; indexRecipe++) {
+        // let foodInfo = res.data.results[indexRecipe]
+        //for ingredient list
+        let ingredientInfo = foodInfo.nutrition.ingredients
+
+        ingredientInfo.forEach((ingredientDetails) => {
+          let ingredientList = document.getElementById("ingredients")
+
+          let newIngredients = document.createElement("p")
+
+          newIngredients.innerHTML = `
+          ${ingredientDetails.amount} ${ingredientDetails.unit} ${ingredientDetails.name}
           `
-        document.getElementById("cocktailIngredient").append(drinkElem)
+          ingredientList.append(newIngredients)
+        })
+        // }
+
+        let totalStepInstructions = ""
+        let instructions = foodInfo.analyzedInstructions[0].steps
+        instructions.forEach((instructionSteps) => {
+          totalStepInstructions += instructionSteps.step
+        })
+        console.log(totalStepInstructions)
+
+        document.getElementById("foodInstructions").innerHTML = `
+          <p>${totalStepInstructions}</p>
+          `
+
+        let nutritionInfo = foodInfo.nutrition.nutrients
+        // console.log("ping" + nutritionInfo[0].name + ": " + nutritionInfo[0].amount)
+        // console.log("ping" + nutritionInfo[8].name + ": " + nutritionInfo[8].amount)
+        // console.log("ping" + nutritionInfo[3].name + ": " + nutritionInfo[3].amount)
+        document.getElementById("calories").innerHTML = `
+          ${nutritionInfo[0].name}: ${nutritionInfo[0].amount} ${nutritionInfo[0].unit}
+          `
+        document.getElementById("fat").innerHTML = `
+          ${nutritionInfo[1].name}: ${nutritionInfo[1].amount} ${nutritionInfo[1].unit}
+          `
+        document.getElementById("carbs").innerHTML = `
+          ${nutritionInfo[3].name}: ${nutritionInfo[3].amount} ${nutritionInfo[3].unit}
+          `
+        document.getElementById("protein").innerHTML = `
+          ${nutritionInfo[8].name}: ${nutritionInfo[8].amount} ${nutritionInfo[8].unit}
+          `
+        document.getElementById("sodium").innerHTML = `
+          ${nutritionInfo[7].name}: ${nutritionInfo[7].amount} ${nutritionInfo[7].unit}
+          `
+        document.getElementById("sugar").innerHTML = `
+          ${nutritionInfo[5].name}: ${nutritionInfo[5].amount} ${nutritionInfo[5].unit}
+          `
+        document.getElementById("cholesterol").innerHTML = `
+          ${nutritionInfo[6].name}: ${nutritionInfo[6].amount} ${nutritionInfo[6].unit}
+          `
+        document.getElementById("fiber").innerHTML = `
+          ${nutritionInfo[13].name}: ${nutritionInfo[13].amount} ${nutritionInfo[13].unit}
+          `
       }
+
     })
+    .catch(err => {
+      console.log(err)
+    })
+})
