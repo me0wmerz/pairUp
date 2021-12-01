0e8e321dc645899efdb66fa333588ab70a39223e
htorres <htorres572@gmail.com>
2021-11-30 23:33:05 -0800

    finished product

--- Assets/savedRecipe.js
+++ Assets/savedRecipe.js
@@ -1,20 +1,17 @@
 let food = JSON.parse(localStorage.getItem("food")) || []
 function displayFood() {
-document.getElementById('foodExample').innerHTML = ''
-let currentIndex = 0
-food.forEach(food => {
-  let foodElem = document.createElement('li')
-  foodElem.innerHTML = `
+  document.getElementById('foodExample').innerHTML = ''
+  let currentIndex = 0
+  food.forEach(food => {
+    let foodElem = document.createElement('li')
+    foodElem.innerHTML = `
   <a href="${food.source}">${food.title}</a>
   <button class="delete" value="${currentIndex}"></button>
   `
-  
-  document.getElementById("foodExample").append(foodElem)
-  currentIndex ++
-
-})
+    document.getElementById("foodExample").append(foodElem)
+    currentIndex++
+  })
 }
-
 
 document.addEventListener('click', event => {
   if (event.target.classList.contains('delete')) {
